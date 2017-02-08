13bec96ab07e473fb171a26b7770021a1e39ba95
rcedermalm <rebecca@cedermalm.se>
2017-02-08 19:26:05 +0100

    Create a window, GLEW and GLFW linked

---
+++ cmake-build-debug/CMakeFiles/OpenGLTry.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for OpenGLTry.
+# This may be replaced when dependencies are built.
---
+++ cmake-build-debug/CMakeFiles/TYGlaDig.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TYGlaDig.dir/main.cpp.obj"
+  "TYGlaDig.pdb"
+  "TYGlaDig.exe"
+  "TYGlaDig.exe.manifest"
+  "libTYGlaDig.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TYGlaDig.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
